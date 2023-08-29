731714602e30261fd3894b568bd4cacc3b672e95
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-08-29 04:39:49 -0700

    fixing the logic in insert function

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,14 +7,11 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node;
 
+	new_node = binary_tree_node(parent, value);
 	if (!new_node || !parent)
 		return (NULL);
-	new_node->n = value;
-	new_node->parent = parent;
-	new_node->right = NULL;
-	new_node->left = NULL;
 	if (parent->left != NULL)
 	{
 		new_node->left = parent->left;
