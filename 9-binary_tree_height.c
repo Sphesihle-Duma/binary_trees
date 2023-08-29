37ce8d162f43cf6051f88749a60f28b848f1200b
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-08-29 12:46:47 -0700

    final fix for binary tree height calculation

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,17 +6,20 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height;
 	size_t left_height;
 	size_t right_height;
 
 	if (!tree)
 		return (0);
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-	if (left_height > right_height)
-		height = left_height + 1;
+	if (tree->left)
+		left_height = 1 + binary_tree_height(tree->left);
 	else
-		height = right_height + 1;
-	return (height - 1);
+		left_height = 0;
+	if (tree->right)
+		right_height = 1 + binary_tree_height(tree->right);
+	else
+		right_height = 0;
+	if (left_height > right_height)
+		return (left_height);
+	return (right_height);
 }
