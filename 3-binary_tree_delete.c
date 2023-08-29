92c9e78473de2400f4ce18531b53bca1a6b8f2c9
Sphesihle-Duma <dumasphesihle22@gmail.com>
2023-08-29 05:48:37 -0700

    added a function to delete the entire tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - deletes the entire tree
+ * @tree: tree to be deleted
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+		return;
+	if (tree)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+	}
+	free(tree);
+}
